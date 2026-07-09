472307abadf15abac680e2728ef716deacbaa0d3
Maheshbabu1408 <mahesh.babumb@yandex.com>
2026-07-09 11:46:37 +0530

    Update connection.js

--- src/connection.js
+++ src/connection.js
@@ -1,1 +1,19 @@
+const {
+  default: makeWASocket,
+  useMultiFileAuthState,
+} = require("@whiskeysockets/baileys");
 
+async function createConnection() {
+  const { state, saveCreds } = await useMultiFileAuthState("./session");
+
+  const sock = makeWASocket({
+    auth: state,
+    printQRInTerminal: true,
+  });
+
+  sock.ev.on("creds.update", saveCreds);
+
+  return sock;
+}
+
+module.exports = { createConnection };
