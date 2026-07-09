beb9ff6e1c8e0364d7e46ade501cfc7b43466317
Maheshbabu1408 <mahesh.babumb@yandex.com>
2026-07-09 12:08:13 +0530

    Update connection.js

--- src/connection.js
+++ src/connection.js
@@ -12,12 +12,24 @@ async function createConnection() {
   const sock = makeWASocket({
     version,
     auth: state,
-    printQRInTerminal: true,
     browser: ["Saviour Ayush Bot", "Chrome", "1.0.0"],
     syncFullHistory: false,
+    printQRInTerminal: false,
   });
 
   sock.ev.on("creds.update", saveCreds);
+
+  sock.ev.on("connection.update", ({ connection, qr }) => {
+    if (connection === "open") {
+      console.log("✅ WhatsApp Connected!");
+    }
+
+    if (qr) {
+      console.log("\n========== QR RECEIVED ==========\n");
+      console.log(qr);
+      console.log("\n===============================\n");
+    }
+  });
 
   return sock;
 }
