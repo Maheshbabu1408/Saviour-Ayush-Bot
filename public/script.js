90afe9d57f54d3c68edb0f4a74e13f2e5fa71bab
Maheshbabu1408 <mahesh.babumb@yandex.com>
2026-07-09 13:18:17 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,1 +1,41 @@
+document.getElementById("pairBtn").addEventListener("click", async () => {
 
+    const number = document.getElementById("number").value;
+
+    if (!number) {
+        alert("Please enter your WhatsApp number");
+        return;
+    }
+
+    document.getElementById("result").innerHTML = "⏳ Generating Pair Code...";
+
+    try {
+
+        const res = await fetch("/pair", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                number
+            })
+        });
+
+        const data = await res.json();
+
+        if (data.code) {
+            document.getElementById("result").innerHTML =
+                "🔑 Pair Code : <br><br><b>" + data.code + "</b>";
+        } else {
+            document.getElementById("result").innerHTML =
+                "❌ " + data.error;
+        }
+
+    } catch (e) {
+
+        document.getElementById("result").innerHTML =
+            "❌ Server Error";
+
+    }
+
+});
