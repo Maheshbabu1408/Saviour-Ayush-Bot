6109b11a478ae5eea633f84a1971c5fd4c5125e7
Maheshbabu1408 <mahesh.babumb@yandex.com>
2026-07-09 12:01:52 +0530

    Update connection.js

--- src/connection.js
+++ src/connection.js
@@ -1,14 +1,20 @@
-const {
+cconst {
   default: makeWASocket,
   useMultiFileAuthState,
+  fetchLatestBaileysVersion,
 } = require("@whiskeysockets/baileys");
 
 async function createConnection() {
   const { state, saveCreds } = await useMultiFileAuthState("./session");
 
+  const { version } = await fetchLatestBaileysVersion();
+
   const sock = makeWASocket({
+    version,
     auth: state,
     printQRInTerminal: true,
+    browser: ["Saviour Ayush Bot", "Chrome", "1.0.0"],
+    syncFullHistory: false,
   });
 
   sock.ev.on("creds.update", saveCreds);
