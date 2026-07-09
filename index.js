da3b6477fb6c0829aaefd71bb26c367325152a24
Maheshbabu1408 <mahesh.babumb@yandex.com>
2026-07-09 11:13:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+const express = require("express");
 const { startBot } = require("./src/bot");
+
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  res.send("✅ Saviour Ayush Hospital Bot is Running");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 startBot();
