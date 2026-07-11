075b99b1fb20abdd7f1fccbf64e253dd6f4fdc40
Maheshbabu1408 <mahesh.babumb@yandex.com>
2026-07-11 14:17:36 +0530

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -1,18 +1,8 @@
 const { createConnection } = require("./connection");
-const handleMessage = require("./message");
 
 async function startBot() {
-  const sock = await createConnection();
-
+  await createConnection();
   console.log("✅ Bot Started");
-
-  sock.ev.on("messages.upsert", async ({ messages }) => {
-    const msg = messages[0];
-
-    if (!msg.message || msg.key.fromMe) return;
-
-    await handleMessage(sock, msg);
-  });
 }
 
 module.exports = { startBot };
